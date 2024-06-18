7a4130c6b3ce4fbc9efda5471b46bffa8bf93162
Erwin Mark <anora.erwinmark@gmail.com>
2024-06-18 16:21:34 +0800

    Updates

---
+++ component---src-pages-page-2-jsx-c42196dd4bf109e49c7d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkerwin_mark_anora_eportfolio=self.webpackChunkerwin_mark_anora_eportfolio||[]).push([[145],{8977:function(e,t,n){n.r(t),n.d(t,{Head:function(){return c}});var a=n(6540),o=n(4794),r=n(6203),l=n(5850);const c=()=>a.createElement(l.A,{title:"Page two"});t.default=()=>a.createElement(r.A,null,a.createElement("h1",null,"Hi from the second page"),a.createElement("p",null,"Welcome to page 2"),a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}]);
+//# sourceMappingURL=component---src-pages-page-2-jsx-c42196dd4bf109e49c7d.js.map
